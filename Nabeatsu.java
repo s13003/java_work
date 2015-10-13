6fd348074428f87ab2ff89f362c77bab0a88e015
s13003 <s13003@std.it-college.ac.jp>
2015-10-13 14:28:32 +0900

    add to Nabeatsu.java

---
+++ Nabeatsu.java
@@ -0,0 +1,25 @@
+class Nabeatsu{
+
+  static final String Three = "Three";
+  static final String check_Three = "3";
+
+  static void Nabeatsu_call(){
+    int x = 0;
+    String temp;
+    while(x < 100){
+      temp = String.valueOf(x);
+      if(temp.indexOf(check_Three) >= 0){
+        System.out.println(Three);
+      }else if(x % 3 == 0){
+        System.out.println(Three);
+      }else{
+        System.out.println(x);
+      }
+    x++;
+    }
+  }
+
+  public static void main(String...args){
+    Nabeatsu_call();
+  }
+}
