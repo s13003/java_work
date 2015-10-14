175dd4e3e902a8d60c8865e3b2a203490d1d5a72
s13003 <s13003@std.it-college.ac.jp>
2015-10-14 11:15:34 +0900

    add to Average.java

---
+++ Average.java
@@ -0,0 +1,24 @@
+class Avarage{
+  /**
+   * @author Daiki GIMA
+   *
+   * 3つの値の平均を求めるメソッドです。
+   *
+   * @return でた平均を返します。
+   */
+
+  static double Ave(){
+    double[] array = {5.5, 10.0, 12.1};
+    double sum = array[0];
+    for(int i = 0; i < array.length-1; i++){
+      sum+=array[i+1];
+    }
+    System.out.print("3つの数の平均値は:" );
+    double ave = sum / array.length;
+    return ave;
+  }
+
+  public static void main(String...args){
+    System.out.println(Ave());
+  }
+}
