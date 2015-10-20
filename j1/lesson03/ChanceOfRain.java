8ba66263226955fba0d6f067388451e5484f6144
s13003 <s13003@std.it-college.ac.jp>
2015-10-20 09:53:48 +0900

    add to 'ChanceOfRain.java' file

---
+++ j1/lesson03/ChanceOfRain.java
@@ -0,0 +1,30 @@
+package j1.lesson03;
+
+import java.util.Scanner;
+
+class ChanceOfRain{
+  public static void main(String...args){
+
+    Scanner stdIn = new Scanner(System.in);
+    int x = 0;
+    boolean flag = false;
+
+    while(!flag){
+      System.out.print("降水確率を入力してください:");
+      x = stdIn.nextInt();
+      if(x >= 0 && x <= 100){
+        flag = true;
+      }
+      if(!flag){
+        System.out.println("降水確率は0から100の間で指定してください。");
+      }
+    }
+    if(x >= 50){
+      System.out.println("傘を持って行ったほうがいいでしょう。");
+    }else if(x < 50 && x >= 20){
+      System.out.println("雨が降るかもしれません。\n傘を持って行ったほうが良いでしょう。");
+    }else{
+      System.out.println("傘は要りません。");
+    }
+  }
+}
